b7b7a5cf2fb316c4c1f943067b8c7cdecf7b00ce
Abdelrahman Iqbal <abdelrahman.iqbal@hotmail.com>
2023-06-27 15:40:03 +1000

    Disable requestMatchers

--- src/main/java/com/example/employees/SecurityConfiguration.java
+++ src/main/java/com/example/employees/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -29,8 +30,9 @@ public class SecurityConfiguration {
         http
                 .csrf((csrf) -> csrf.disable())
                 .authorizeRequests()
-                .requestMatchers("/employees/**").hasRole("USER")
-                .requestMatchers("/contacts/**").hasRole("ADMIN")
+//                .requestMatchers("/employees/create/**").hasRole("USER")
+//                .requestMatchers("/employees/**").hasRole("USER")
+//                .requestMatchers("/contacts/**").hasRole("ADMIN")
                 .requestMatchers("/**").permitAll();
         return http.build();
     }
